a993babc606cba6ae03b93979fac82ef1f0f1730
wangbinghan1212-hash <wangbinghan1212@icloud.com>
2026-07-03 06:01:57 +0800

    更新 database.py

--- database.py
+++ database.py
@@ -1,1 +1,14 @@
+import json
+import os
 
+DB_FILE = "data.json"
+
+def load_data():
+    if not os.path.exists(DB_FILE):
+        return {}
+    with open(DB_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def save_data(data):
+    with open(DB_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
